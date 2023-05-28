4fb09bc5d20d894659eef4782e514498c8798024
Nicolás Londoño <nicksiuxs@gmail.com>
2023-05-28 10:16:00 -0500

    Add: login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import Header from "./components/shared/Header";
 
 const App = () => {
   return (
-    <AppRoutes />
+    <>
+      <Header />
+      <AppRoutes />
+    </>
   );
 };
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Header from "../../components/shared/Header";
+
+import Logo from "../../assets/contacme-logo.png"
 import { useSelector } from "react-redux"
+import { Navigate, useNavigate } from "react-router-dom";
+
 
 const Login = () => {
 
   const user = useSelector(state => state.user)
-  console.log(user)
+
+  const navigate = useNavigate();
+  const handleNavigate = (route) => {
+    navigate(route)
+  }
   return (
-    <>
-      <Header />
+    <main>
       <div>
-        <h1>Login Page s</h1>
+        <img src={Logo} />
+        <h1>Ingrese como</h1>
+        <div>
+          <button onClick={() => handleNavigate("login/client")}>Cliente</button>
+          <br />
+          <button onClick={() => handleNavigate("login/supplier")}>Provedor</button>
+        </div>
       </div>
-    </>
+    </main>
   );
 };
 
