21f01a0e36f03d66cec18ec53773f77a6d65704f
Nicolás Londoño <nicksiuxs@gmail.com>
2023-05-29 10:28:03 -0500

    Add: login supplier

--- src/pages/Supplier/LoginSupplier.jsx
+++ src/pages/Supplier/LoginSupplier.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../context/thunks/userThunks";
+import LoginForm from "../../components/shared/LoginForm";
 
 const LoginSupplier = () => {
-  return (
-    <div>
-      <h1>LoginSupplier</h1>
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  const handleSubmit = (email, password) => {
+    dispatch(loginUser(email, password))
+  }
+
+  return <div>
+    <LoginForm title="Iniciar sesión como Proveedor" route={"/create/supplier"} handleSubmit={handleSubmit} />
+  </div>;
 };
 
 export default LoginSupplier;
