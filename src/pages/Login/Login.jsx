d6d021a7a82a03bd5acaa9310c1f63f00a0b24c7
Nicolás Londoño <nicksiuxs@gmail.com>
2023-05-29 22:01:46 -0500

    Add: login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import Header from "../../components/shared/Header";
+
+import Title from "../../components/shared/Title";
+import Button from "../../components/shared/Button";
 
 import Logo from "../../assets/contacme-logo.png"
-import { useSelector } from "react-redux"
-import { Navigate, useNavigate } from "react-router-dom";
 
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
-  const user = useSelector(state => state.user)
-
   const navigate = useNavigate();
+
   const handleNavigate = (route) => {
     navigate(route)
   }
+
   return (
-    <main>
-      <div>
-        <img src={Logo} />
-        <h1>Ingrese como</h1>
-        <div>
-          <button onClick={() => handleNavigate("login/client")}>Cliente</button>
-          <br />
-          <button onClick={() => handleNavigate("login/supplier")}>Provedor</button>
+    <main className="p-3 h-screen flex justify-center items-center max-h-[90vh]">
+      <div className="flex flex-col justify-center items-center w-full border border-dashed h-fit px-3 py-5 rounded">
+        <img src={Logo} className="w-14 mb-6" />
+        <Title title={"Ingrese como"} styles={"mb-10"} />
+        <div className="w-full">
+          <Button label="Cliente" onClick={() => handleNavigate("login/client")} styles={"mb-7"} />
+          <Button label="Provedor" onClick={() => handleNavigate("login/supplier")} />
         </div>
       </div>
     </main>
