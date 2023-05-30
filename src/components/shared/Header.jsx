1a39168a589854403197aa88b991d6412522a1a4
Nicolás Londoño <nicksiuxs@gmail.com>
2023-05-29 21:25:39 -0500

    Fix: header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Logo from "../../assets/contacme-logo.png"
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 flex items-center justify-between">
-      <img src={Logo} />
-      <nav>
+    <header className="bg-gray-800 flex items-center sm : justify-center p-3">
+      <div >
+        <Link to="/" className="flex items-center">
+          <img src={Logo} />
+          <h1 className="text-white ml-2">ContactMe</h1>
+        </Link>
+      </div>
+      <nav className="sm : hidden">
         <a href="#">Publicaciones</a>
         <a href="#">Mensajes</a>
       </nav>
-      <div>
+      <div className="sm : hidden">
         <p>User</p>
       </div>
     </header>
